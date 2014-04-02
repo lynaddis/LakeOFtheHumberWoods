bad7e03b496d53a176bd2d4da5e2ce3a15077259
lynaddis <lynaddis@hotmail.com>
2014-04-02 11:09:56 -0400

    script

---
+++ LofW/dbo.suggestion.data.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [dbo].[suggestion] ON
+INSERT INTO [dbo].[suggestion] ([Id], [first], [last], [phone], [hours], [email], [comment], [suggestionType]) VALUES (5, N'jane', N'doe', N'545445', N'Morning', N'jdoe@hotmail.com', N'ggggggggggggggggggggggggggggggggggggggggggggggggggggg', N'Other')
+INSERT INTO [dbo].[suggestion] ([Id], [first], [last], [phone], [hours], [email], [comment], [suggestionType]) VALUES (8, N'test', N'test', N'1234567890', N'Morning', N'test@test.ca', N'dtrdtdut', N'Complaint')
+INSERT INTO [dbo].[suggestion] ([Id], [first], [last], [phone], [hours], [email], [comment], [suggestionType]) VALUES (12, N'Harry', N'Jones', N'2899233349', N'Afternoon', N'lynaddis@hotmail.com', N'the sky is blue', N'Suggestion')
+INSERT INTO [dbo].[suggestion] ([Id], [first], [last], [phone], [hours], [email], [comment], [suggestionType]) VALUES (16, N'LYN-ADDIS', N'LYNCH', N'289-923-3349', N'Afternoon', N'lynaddis@hotmail.com', N'hjddjjkhd', N'Suggestion')
+INSERT INTO [dbo].[suggestion] ([Id], [first], [last], [phone], [hours], [email], [comment], [suggestionType]) VALUES (18, N'LYNADDIS', N'LYNCH', N'289-923-3349', N'Morning', N'lynaddis@hotmail.com', N'hh', N'Suggestion')
+SET IDENTITY_INSERT [dbo].[suggestion] OFF
