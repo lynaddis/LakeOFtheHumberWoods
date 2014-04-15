2402bd68433c3e97a846672150d0ac8961468fce
JoelLaCroix <joel.lacroix1@gmail.com>
2014-04-15 16:54:23 -0400

    class  = super new

---
+++ LofW/ACTUAL HOSPITAL FILES !!!!!!/hospital/cancel.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="cancel.aspx.cs" Inherits="cancel" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<script type="text/javascript">
+    alert("You have chosen to cancel the Paypal Payment process;The Transaction has been stopped and you will be redirected tothe hospital home page!'");
+    window.location ="http://daniellestirling.com/hospital/home.aspx";
+</script>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ LofW/ACTUAL HOSPITAL FILES !!!!!!/hospital/cancel.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class cancel : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //string redirectTo = "http://daniellestirling.com/hospital/home.aspx";
+        //string script = "<script>";
+        //script += "alert('You have chosen to cancel the Paypal Payment process;The Transaction has been stopped and you will be redirected tothe hospital home page!');";
+        //script += "window.location='" + redirectTo + "'</script>";
+
+
+        //ScriptManager.RegisterStartupScript(this, typeof(Page), "test", script, true);
+
+    }
+}
