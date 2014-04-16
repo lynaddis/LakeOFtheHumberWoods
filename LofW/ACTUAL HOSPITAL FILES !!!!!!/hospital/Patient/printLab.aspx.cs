e70ff34763af72a9b9cc8f8bf156011e90397606
lynaddis <lynaddis@hotmail.com>
2014-04-16 12:08:27 -0400

    ?

---
+++ LofW/ACTUAL HOSPITAL FILES !!!!!!/hospital/Patient/printLab.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Patient_printLab : System.Web.UI.Page
+{
+    labClass objLinq = new labClass();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+
+            if (!string.IsNullOrEmpty(Request.QueryString["param"]))
+            {
+                dv_print.DataSource = objLinq.getLabsByID(Convert.ToInt32(Request.QueryString["param"].ToString()));
+                dv_print.DataBind();
+            }
+
+        }
+    }
+}
